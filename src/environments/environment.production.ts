c8efc7488c01f6de1bb0b05ed0e94a44b68af146
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-12-18 00:27:14 +0530

    security url updated

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,6 +3,6 @@ export const environment = {
   envName: 'prod',
   // In production, set a full URL if your app is served from a different domain than the API
   // e.g. 'https://api.securechat.example.com'
-  apiBaseUrl: 'project-secure-chat-backend.kussagrapathak.com/api',
+  apiBaseUrl: 'https://project-secure-chat-backend.kussagrapathak.com/api',
   wsUrl: '/socket.io'
 };
